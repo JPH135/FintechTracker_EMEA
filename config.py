37f44b9e4ba178e37399370b62c049b4e14d66ae
JPH135 <james.hayward@gs.com>
2026-03-23 18:07:52 +0000

    Update feed URLs to include angle brackets

--- config.py
+++ config.py
@@ -4,32 +4,32 @@ LOOKBACK_DAYS = 7
 FEEDS = [
     {
         "name": "Sifted Fintech",
-        "url": "https://sifted.eu/sector/fintech/feed",
+        "url": "<https://sifted.eu/sector/fintech/feed>",
         "region": "Europe",
     },
     {
         "name": "Finextra",
-        "url": "https://www.finextra.com/rss/rss.aspx",
+        "url": "<https://www.finextra.com/rss/rss.aspx>",
         "region": "Global/EMEA",
     },
     {
         "name": "The Fintech Times",
-        "url": "https://thefintechtimes.com/feed",
+        "url": "<https://thefintechtimes.com/feed>",
         "region": "Global/UK",
     },
     {
         "name": "Fintech Nordics",
-        "url": "https://fintechnordics.com/feed",
+        "url": "<https://fintechnordics.com/feed>",
         "region": "Nordic",
     },
     {
         "name": "Fintech Switzerland",
-        "url": "https://fintechnews.ch/feed",
+        "url": "<https://fintechnews.ch/feed>",
         "region": "Switzerland",
     },
     {
         "name": "TechCrunch Fintech",
-        "url": "https://techcrunch.com/tag/fintech/feed/",
+        "url": "<https://techcrunch.com/tag/fintech/feed/>",
         "region": "Global",
     },
 ]
